8641a59ff63b19cb0be61c6a54c2fa19b628fc06
sejalkulkarni <sejalkulkarni017@gmail.com>
2026-01-28 18:18:46 +0530

    interfaces added

---
+++ HRApp/interfaces/IApprisable.js
@@ -0,0 +1,8 @@
+
+class IAppraisable {
+
+    conductAppraisal() {
+        throw new Error('Method "conductAppraisal()" must be implemented.');
+    }
+}
+module.exports = IAppraisable;
---
+++ HRApp/interfaces/ITrainer.js
@@ -0,0 +1,13 @@
+
+class ITrainer {
+
+
+    conductTraining() {
+        throw new Error('Method "conductTraining()" must be implemented.');
+    }
+    conductHandsOnSession() {
+        throw new Error('Method "conductHandsOnSession()" must be implemented.');
+    }
+}
+
+module.exports = ITrainer;
